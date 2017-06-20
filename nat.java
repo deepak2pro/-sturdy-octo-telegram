aa72d0d56241eeae16fe91f58b2b40b1acc0c4c1
deepak2pro <draj4060@gmail.com>
2017-06-20 14:42:37 +0530

    Create nat.java

---
+++ nat.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class nat
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int sum=0;
+int n=s.nextInt();
+for(int i=0;i<n;i++)
+{
+sum+=i;
+}
+}
+}
