3b1b1631200da949a22b884397dc5f454057f737
deepak2pro <draj4060@gmail.com>
2017-06-20 09:55:42 +0530

    Rename check.java to vowel.java

---
+++ vowel.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class check
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+if(str=='a'||str=='i'||str=='e'||str=='o'||str=='u')
+System.out.print("vowel");
+else
+System.out.rint("constant");
+}
+}
