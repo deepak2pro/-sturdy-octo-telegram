2f2c7540bd0f378331a12002e78c791e9f3d1775
deepak2pro <draj4060@gmail.com>
2017-06-21 10:17:00 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class prime
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+if(a/a==1)
+System.out.print("prime"):
+else
+sytdem.out.print("not");
+}
+}
