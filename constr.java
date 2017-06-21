df4e6b4d49fc5d28d239df30ee8eb56684c0eb79
deepak2pro <draj4060@gmail.com>
2017-06-21 12:20:20 +0530

    Create constr.java

---
+++ constr.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class constr
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+String str=s.nextLine();
+int i=Integer.parseInt(str);
+System.out.print(i);
+}
+}
