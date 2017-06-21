f1cdb92fc3a09ebd023ea098db45513b2ff845c2
deepak2pro <draj4060@gmail.com>
2017-06-21 10:33:20 +0530

    Update odd.java

--- odd.java
+++ odd.java
@@ -6,9 +6,13 @@ public static void main(String[] args)
 {
 Scanner s=new Scanner(System.in);
 int a=s.nextInt();
-if(a%2!=0)
-System.out.print("odd");
+int b=s.nextInt();
+for(int i=a;i<b;i++)
+{
+if(i%2!=0)
+System.out.print(i);
 else
 System.out.print("even");
+}
 }
 }
