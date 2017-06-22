abfde6a1e966f379f05c60f772ed01de9404a9d0
deepak2pro <draj4060@gmail.com>
2017-06-22 11:41:32 +0530

    Create work.java

---
+++ work.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class work
+{
+    public static void main(String[] arg)
+    {
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine().toLowerCase();
+        String str_2="sunday";
+        if(str.equals(str_2))
+         System.out.print("false");
+        else
+         System.out.print("true");
+    }
+}
